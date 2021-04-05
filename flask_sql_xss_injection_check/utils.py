fa9f929c188d52437924f13149cba5905e13cbcd
deepak singh <deepaksinghjadon@gmail.com>
2021-04-05 09:41:41 +0530

    ref the source for regex

--- flask_sql_xss_injection_check/utils.py
+++ flask_sql_xss_injection_check/utils.py
@@ -3,7 +3,11 @@ import logging
 
 LOG = logging.getLogger(__name__)
 
+
 def has_sql(content):
+    """
+    regex reference: http://www.symantec.com/connect/articles/detection-sql-injection-and-cross-site-scripting-attacks
+    """
     match = (
         re.search(r'(\%27)|(\')|(\-\-)|(\%23)|(#)', content, re.IGNORECASE) or
         re.search(r'((\%3D)|(=))[^\n]*((\%27)|(\')|(\-\-)|(\%3B)|(;))', content, re.IGNORECASE) or
@@ -18,6 +22,9 @@ def has_sql(content):
     return False
 
 def has_xss(content):
+    """
+    regex reference: http://www.symantec.com/connect/articles/detection-sql-injection-and-cross-site-scripting-attacks
+    """
     match = (
         re.search(r'((\%3C)|<)((\%2F)|\/)*[a-z0-9\%]+((\%3E)|>)', content, re.IGNORECASE) or
         re.search(r'((\%3C)|<)((\%69)|i|(\%49))((\%6D)|m|(\%4D))((\%67)|g|(\%47))[^\n]+((\%3E)|>)', content, re.IGNORECASE) or
